5fcf554ec7eb00d6966aa12f29a7eca036aaff5d
Ian <smithdevendeavor@gmail.com>
2020-05-25 12:36:45 -0400

    small updates to style.css and index.html scaffolding

--- assets/scripts/team-planner.js
+++ assets/scripts/team-planner.js
@@ -1,18 +1,22 @@
-const teamPlaceholder = new Pokedex.Pokedex();
+const dex = new Pokedex.Pokedex();
 const plannerSlot = document.getElementById("selected-slot");
 //placeholder default sprite
-var pokeballSpriteUrl;
-var sloti= "";
+var defaultSpriteUrl;
 var selectedPokeImgUrl;
 var selectedPokeName="";
 
+const changeValue = () => {
+    document.getElementById("pokemon1").value = "Foo Bar";
+}
 
+
+/*obtains pokemon information to be displayed for the selected pokemon*/
 const displaySelectedPokemon = (pokemon) => {
-    teamPlaceholder.getPokemonByName(pokemon)
+    dex.getPokemonByName(pokemon)
     .then(function(response) {
         selectedPokeImgUrl = response.sprites["front_default"]
         selectedPokeName = response.name;
-        document.getElementById("selected-label").textContent = selectedPokeName; 
+        document.getElementById("selected-label").value = selectedPokeName; 
         document.getElementById("selected-img").src= selectedPokeImgUrl;
         document.getElementById("pokemon1").text = selectedPokeName;
     });
@@ -21,29 +25,40 @@ const displaySelectedPokemon = (pokemon) => {
 }
 
 
-/*utilizes "getItemByName()" method to obtain the default sprite image 
-of a pokeball. Once obtained, the url response is set as the src for
-all 6 of the team slots images  */
+/*utilizes "getItemByName()" method to obtain the default sprite 
+image. Once obtained, the url response is set as the src for
+all 6 of the team slots images with a for loop  */
 const initDefaultPlaceHolders = () => {
-    teamPlaceholder.getItemByName("poke-ball")
+    var imgi= "";
+    dex.getPokemonFormByName("unown-question")
     .then(function(response) {
-        pokeballSpriteUrl = response.sprites["default"];
+        console.log(response);
+        defaultSpriteUrl = response.sprites["front_default"];
         for(i = 1; i <= 6; i++){
-            sloti = document.getElementById("slot-" + i).src = pokeballSpriteUrl;
+            imgi = document.getElementById("img-" + i).src = defaultSpriteUrl;
         }
     });
     
     for(i = 1; i <= 6; i++){
-        sloti.src = pokeballSpriteUrl;
+        imgi.src = defaultSpriteUrl;
     }
+    
 }
 
-const changeSprite = () => {
-    document.getElementById("slot-1").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/master-ball.png";
+const selectPokemon = (pokemon) => {
+    var name = "";
+    dex.getPokemonByName(pokemon)
+    .then(function(response) {
+        name = response.name;
+    
+    });
+    
+    displaySelectedPokemon(name);
 }
+
 
 initDefaultPlaceHolders();
-displaySelectedPokemon("bulbasaur");
+
 
 
 
